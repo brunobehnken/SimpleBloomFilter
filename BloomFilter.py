07d49abbfd891e26e22310bc51b878de2fab9830
brunobehnken <brunobehnken@yahoo.com.br>
2018-11-10 20:36:28 -0200

    method __get_pos created, methods add_element and check_element implemented

--- BloomFilter.py
+++ BloomFilter.py
@@ -5,15 +5,29 @@ class BloomFilter:
 
     def __init__(self, size=1000, gran=1):
         self.bfilter = [False] * size  # The filter itself
-        self.gran = gran
+        self.size = size  # size of the filter
+        self.gran = gran  # Granularity of the filter
 
-    def addElement(self, elem):
-        # TODO magic hashing
-        pass
+    def __get_pos(self, elem, i):
+        hashcode = hash((elem, i))
+        while hashcode >= self.size:
+            resto = hashcode % self.size
+            hashcode = hashcode // self.size
+            hashcode += resto
+        return hashcode
 
-    def checkElement(self, elem):
-        # TODO magic hashing
-        pass
+    def add_element(self, elem):
+        for i in range(0, self.gran):
+            pos = self.__get_pos(elem, i)
+            if not self.bfilter[pos]:
+                self.bfilter[pos] = True
+
+    def check_element(self, elem):
+        for i in range(0, self.gran):
+            pos = self.__get_pos(elem, i)
+            if not self.bfilter[pos]:
+                return False
+        return True
 
 
 def main():
@@ -23,12 +37,12 @@ def main():
 
     for i in range(0, 400):
         elem = randint(0, 1000)
-        b_filter.addElement(elem)
+        b_filter.add_element(elem)
         control_group.add(elem)
 
     for i in range(0, 400):
         elem = randint(0, 1000)
-        if b_filter.checkElement(elem):
+        if b_filter.check_element(elem):
             if elem in control_group:
                 true_positives += 1
             else:
